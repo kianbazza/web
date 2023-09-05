48123fff6824fd591a14418605a70985c80ba290
Kian Bazza <bazarjani.kian@gmail.com>
2023-09-05 18:12:56 -0400

    Deploy initial version of site

--- app/(tldr)/layout.tsx
+++ app/(tldr)/layout.tsx
@@ -19,10 +19,17 @@ export default function RootLayout({
         className={cn(
           jetbrains_mono.variable,
           inter.variable,
-          "min-h-screen max-w-xl flex flex-col m-auto p-10"
+          "min-h-screen max-w-xl flex flex-col m-auto py-10 px-7"
         )}
       >
-        {children}
+        <div className="flex-1">{children}</div>
+        <footer className={cn("text-slate-400 text-sm flex flex-col")}>
+          <div>Copyright © 2023 by Kian Bazarjani.</div>
+
+          <span className="sm:whitespace-pre">
+            Built with ❤️ // Next.js, shadcn/ui, Framer Motion.
+          </span>
+        </footer>
       </body>
     </html>
   )
--- app/(tldr)/page.tsx
+++ app/(tldr)/page.tsx
@@ -3,24 +3,19 @@
 import { Button } from "@/components/ui/button"
 import { Archive, FileText, Github, Linkedin } from "lucide-react"
 import TypingAnimationText from "./_components/TypingAnimationText"
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from "@/components/ui/collapsible"
-import { Badge } from "@/components/ui/badge"
+import "./styles.css"
 
 const roles = [
   "web developer",
   "cloud engineer",
   "infrastructure architect",
-  "web/cloud/infrastructure developer",
+  // "web/cloud/infrastructure developer",
 ]
 
 export default function TldrPage() {
   return (
-    <div className="flex flex-col gap-5">
-      <div className="flex items-center justify-between">
+    <div className="flex flex-col gap-10">
+      <div className="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
         <div className="flex flex-col">
           <h1 className="text-3xl font-black tracking-tighter">
             Kian Bazarjani
@@ -32,17 +27,23 @@ export default function TldrPage() {
             kian@bazza.dev
           </a>
           <a className="text-sm pt-2">
-            <TypingAnimationText texts={roles} loop={false} /> @{" "}
+            <TypingAnimationText texts={roles} loop={true} /> @{" "}
             <span className="align-middle">🇨🇦</span>
           </a>
         </div>
-        <div className="flex gap-1 sm:gap-2">
+        <div className="flex gap-4">
           <Button
             className="hover:bg-slate-100 rounded-xl h-7 w-7"
             variant="ghost"
-            size="icon"
+            // size="icon"
           >
-            <FileText className="h-4 w-4" />
+            <a
+              href="https://go.bazza.dev/resume"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FileText className="h-4 w-4" />
+            </a>
           </Button>
           <Button
             className="hover:bg-slate-100 rounded-xl h-7 w-7"
@@ -61,22 +62,108 @@ export default function TldrPage() {
         </div>
       </div>
       <div>
+        {/* <h2 className="text-2xl font-bold tracking-tighter mb-3">About Me</h2> */}
+        {/* <div className="flex flex-col gap-2 text-xs">
+          <p className="shared receive">Hey!</p>
+          <p className="shared send">
+            Hey! I'm <span className="underline">Kian</span> 😊.
+          </p>
+          <p className="shared receive">What's your background?</p>
+          <p className="shared send">
+            Graduating with a degree in <b>Computer Science</b>, I've always had
+            a passion for building things - starting with{" "}
+            <i>complex redstone machinery</i> in Minecraft, all the way to{" "}
+            <span className="underline">full-stack web development</span> in{" "}
+            <b>Next.js</b>.
+          </p>
+          <p className="shared receive">Cool! What are you doing now?</p>
+          <p className="shared send">
+            I'm currently working for <b>National Defence</b> and expanding my
+            skills in <span className="underline">web development</span>,{" "}
+            <span className="underline">cloud engineering</span> (AWS), and{" "}
+            <span className="underline">infrastructure architecture</span>.
+          </p>
+        </div> */}
+        <div className="flex flex-col gap-2 text-sm">
+          <p className="shared receive">
+            hey! i'm <span className="underline">kian</span> 😊
+          </p>
+          {/* <p className="shared send">
+            I've always had a passion for building things - from creating{" "}
+            <i>redstone contraptions</i> in Minecraft, all the way to{" "}
+            <span className="underline">full-stack web development</span> in{" "}
+            <b>Next.js</b>.
+          </p> */}
+          <p className="shared receive">
+            i'm currently working for <b>National Defence</b> and expanding my
+            skills in <span className="underline">web development</span>,{" "}
+            <span className="underline">cloud engineering</span> (AWS), and{" "}
+            <span className="underline">infrastructure architecture</span>.
+          </p>
+          <p className="shared receive">
+            feel free to explore my projects, or{" "}
+            <a
+              className="underline font-bold decoration-2 decoration-red-600/30 hover:decoration-red-600/70"
+              href="https://go.bazza.dev/resume"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              if you're here for my resume, click here!
+            </a>{" "}
+            👈
+          </p>
+
+          <p className="shared receive">cheers! 🍻</p>
+        </div>
+      </div>
+      <div>
         <h2 className="text-2xl font-bold tracking-tighter mb-3">Projects</h2>
         <div className="flex flex-col gap-2 text-sm">
           <div>
-            <div className="inline-flex items-center gap-2">
-              <span>Optimal Enchant Tool</span>
+            <div className="flex flex-col gap-1">
+              <div className="flex gap-3 items-center">
+                <a
+                  className="underline"
+                  href="https://github.com/bazzadev/optimalenchanttool"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  Optimal Enchant Tool
+                </a>
+                {/* <div>
+                  <Badge className="font-medium" variant="secondary">
+                    Next.js
+                  </Badge>
+                </div> */}
+              </div>
+              <p className="text-gray-500">
+                Optimize combining things in an anvil in Minecraft.
+              </p>
             </div>
           </div>
           <div>
-            <div className="inline-flex items-center gap-2">
-              <span>Simple Menus</span>
+            <div className="inline-flex items-center gap-2 text-amber-700 opacity-50">
+              <a
+                className="underline"
+                href="https://github.com/bazzadev/simplemenus"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Simple Menus
+              </a>
               <Archive className="h-4 w-4" />
             </div>
           </div>
           <div>
-            <div className="inline-flex items-center gap-2">
-              <span>DeltaCore</span>
+            <div className="inline-flex items-center gap-2 text-amber-700 opacity-50">
+              <a
+                className="underline"
+                href="https://github.com/bazzadev/deltacore"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                DeltaCore
+              </a>
               <Archive className="h-4 w-4" />
             </div>
           </div>
