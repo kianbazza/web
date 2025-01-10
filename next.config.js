a30307e0ea7a51fa1749935418e3a7997990ab53
Kian Bazza <kianbazza@Kians-MacBook-Pro.local>
2025-01-09 23:16:24 -0500

    Add rewrites to passgen app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/toolbox/passgen',
+        destination: `${process.env.NEXT_PUBLIC_PASSGEN_URL}/toolbox/passgen`,
+      },
+      {
+        source: '/toolbox/passgen/:path*',
+        destination: `${process.env.NEXT_PUBLIC_PASSGEN_URL}/toolbox/passgen/:path*`,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
