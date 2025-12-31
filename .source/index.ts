bcf74e3ee806762c33ccd95ac9cd0784f27d78af
Kian Bazarjani <kian@tryprofound.com>
2025-12-31 09:54:43 -0500

    upgrade to next v16.1

--- .source/index.ts
+++ .source/index.ts
@@ -1,25 +1,6 @@
 // @ts-nocheck -- skip type checking
-
-import { _runtime } from 'fumadocs-mdx/runtime/next'
-import * as craft_0 from '../content/craft/cursor-origin.mdx?collection=craft&hash=1763180323771'
-import * as craft_1 from '../content/craft/video-player.mdx?collection=craft&hash=1763180323771'
-import type * as _source from '../source.config'
-export const craft = _runtime.docs<typeof _source.craft>(
-  [
-    {
-      info: {
-        path: 'cursor-origin.mdx',
-        fullPath: 'content/craft/cursor-origin.mdx',
-      },
-      data: craft_0,
-    },
-    {
-      info: {
-        path: 'video-player.mdx',
-        fullPath: 'content/craft/video-player.mdx',
-      },
-      data: craft_1,
-    },
-  ],
-  [],
-)
+import * as craft_1 from "../content/craft/video-player.mdx?collection=craft&hash=1763180323771"
+import * as craft_0 from "../content/craft/cursor-origin.mdx?collection=craft&hash=1763180323771"
+import { _runtime } from "fumadocs-mdx/runtime/next"
+import * as _source from "../source.config"
+export const craft = _runtime.docs<typeof _source.craft>([{ info: {"path":"cursor-origin.mdx","fullPath":"content/craft/cursor-origin.mdx"}, data: craft_0 }, { info: {"path":"video-player.mdx","fullPath":"content/craft/video-player.mdx"}, data: craft_1 }], [])
